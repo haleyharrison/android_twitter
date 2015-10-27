87aa0870b8c94e5ea42866b6173bbf10144caa12
Molly Waggett <mwaggett@alumni.reed.edu>
2015-10-27 10:04:39 -0700

    Set up Active Android.

---
+++ app/src/main/java/com/epicodus/twitter_clone/ui/MainActivity.java
@@ -0,0 +1,14 @@
+package com.epicodus.twitter_clone.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import com.epicodus.twitter_clone.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
